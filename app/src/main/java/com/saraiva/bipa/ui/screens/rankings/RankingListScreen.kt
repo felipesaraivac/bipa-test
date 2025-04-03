518543c6a618558d9215a64f1b1da8337d6d2387
Felipe Saraiva <felipesaraivac@outlook.com>
2025-04-03 15:23:01 -0300

    Fixed scroll when error ocurred

--- app/src/main/java/com/saraiva/bipa/ui/screens/rankings/RankingListScreen.kt
+++ app/src/main/java/com/saraiva/bipa/ui/screens/rankings/RankingListScreen.kt
@@ -2,11 +2,16 @@
 
 package com.saraiva.bipa.ui.screens.rankings
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.Sort
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -18,11 +23,15 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.material3.pulltorefresh.PullToRefreshState
+import androidx.compose.material3.pulltorefresh.pullToRefresh
 import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.input.pointer.motionEventSpy
 import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.saraiva.bipa.R
@@ -67,11 +76,16 @@ fun RankingListScreen(
                 .padding(paddingValues)
                 .fillMaxSize(),
         ) {
-            RankingContent(
-                modifier,
-                state,
+            PullToRefreshBox(
+                state = rememberPullToRefreshState(),
                 isRefreshing = refreshState,
-                onRefresh = { viewModel.refresh() })
+                onRefresh = { viewModel.refresh() }
+            ) {
+                RankingContent(
+                    modifier,
+                    state,
+                )
+            }
         }
     }
 }
@@ -80,13 +94,20 @@ fun RankingListScreen(
 fun RankingContent(
     modifier: Modifier = Modifier,
     state: State<List<NodeEntity>>,
-    isRefreshing: Boolean,
-    onRefresh: () -> Unit
+    onRefresh: () -> Unit = {},
 ) {
-    val refreshState = rememberPullToRefreshState()
     when (state) {
         is State.Failed -> {
-            Text(text = state.message)
+
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .verticalScroll(rememberScrollState()),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Text(stringResource(R.string.data_could_not_be_loaded))
+            }
         }
 
         is State.Loading -> ProgressBar()
@@ -95,9 +116,6 @@ fun RankingContent(
             RankingList(
                 modifier = modifier,
                 nodes = state.data,
-                onRefresh = onRefresh,
-                isRefreshing = isRefreshing,
-                refreshState = refreshState
             )
         }
     }
@@ -107,22 +125,12 @@ fun RankingContent(
 fun RankingList(
     nodes: List<NodeEntity>,
     modifier: Modifier = Modifier,
-    onRefresh: () -> Unit,
-    isRefreshing: Boolean,
-    refreshState: PullToRefreshState
 ) {
-    PullToRefreshBox(
-        state = refreshState,
-        isRefreshing = isRefreshing,
-        onRefresh = onRefresh
+    LazyColumn(
+        modifier = modifier
     ) {
-
-        LazyColumn(
-            modifier = modifier
-        ) {
-            itemsIndexed(nodes) { index, node ->
-                CardHorizontalRanking(node = node, position = index + 1)
-            }
+        itemsIndexed(nodes) { index, node ->
+            CardHorizontalRanking(node = node, position = index + 1)
         }
     }
 }
